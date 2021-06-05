f29e0e2c3f9ca58fd2aba654ae60d49ded46612b
Kiran S Baliga <60563196+kiransbaliga@users.noreply.github.com>
2021-06-05 12:01:27 +0530

    Create Lab2_5.sh

---
+++ Lab2_5.sh
@@ -0,0 +1,14 @@
+no=1
+col=0
+
+while [ $col -lt 4 ]
+do
+    row=0
+    while [ $row -lt `expr $col + 1` ]
+    do   echo -n "$no "
+    no=`expr $no + 1`
+    row=`expr $row + 1`
+    done
+    echo ""
+    col= `expr $col + 1`
+done
