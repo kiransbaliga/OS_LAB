27565178341f2a72569b6350503cb65d33eac158
Kiran S Baliga <60563196+kiransbaliga@users.noreply.github.com>
2021-06-06 20:11:33 +0530

    Update Lab2_5.sh

--- Lab2_5.sh
+++ Lab2_5.sh
@@ -1,6 +1,15 @@
 no=1
 col=0
 
-for (( i=1 ; i < 5; i++ ))
+while [ $col -lt 4 ]
 do
-    for (( i=1 ; i <= $i; i++ )) 
+    row=0
+    while [ $row -lt `expr $col + 1` ]
+    do   
+        echo -n "$no "
+        no=`expr $no + 1`
+        row=`expr $row + 1`
+    done
+    echo ""
+    col= `expr $col + 1`
+done
